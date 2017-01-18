a94183d27e0e20caf9111e3eb16b3d8ba8182365
LiudmylaPatenko <lubilla03@gmail.com>
2017-01-18 23:03:07 +0200

    add image adapter class(bind only image and username)

---
+++ app/src/main/java/com/example/liudmula/photographer/ImageAdapter.java
@@ -0,0 +1,53 @@
+package com.example.liudmula.photographer;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by liudmula on 18.01.17.
+ */
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+
+    private ArrayList<Photo> mPhotos;
+
+    public ImageAdapter(ArrayList<Photo> photos){
+        mPhotos = photos;
+    }
+
+    @Override
+    public ImageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_image_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ImageAdapter.ViewHolder holder, int pos) {
+        holder.mIvRow.setImageBitmap(mPhotos.get(pos).getTestBitmap());
+        holder.mTvRow.setText(mPhotos.get(pos).getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPhotos.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView mIvRow;
+        private TextView mTvRow;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mIvRow = (ImageView)itemView.findViewById(R.id.iv_card_row);
+            mTvRow = (TextView)itemView.findViewById(R.id.tv_card_row);
+        }
+    }
+}
