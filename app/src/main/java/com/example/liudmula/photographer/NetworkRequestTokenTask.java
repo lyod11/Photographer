e272cb9bb58f061fc2a5ed5a63357f49451a625b
LiudmylaPatenko <lubilla03@gmail.com>
2017-01-20 17:28:05 +0200

    add acync task for token request

---
+++ app/src/main/java/com/example/liudmula/photographer/NetworkRequestTokenTask.java
@@ -0,0 +1,29 @@
+package com.example.liudmula.photographer;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by liudmula on 20.01.17.
+ */
+
+public class NetworkRequestTokenTask extends AsyncTask<String, Void, AuthToken>{
+
+    public interface RequestTokenListener{
+        void onRequestTokenSuccessful(AuthToken token);
+        void onRequestTokenError();
+    }
+
+    private RequestTokenListener mListener;
+
+    NetworkRequestTokenTask(RequestTokenListener listener){
+        mListener = listener;
+    }
+
+    @Override
+    protected AuthToken doInBackground(String... params) {
+        if(params == null){
+            return null;
+        }
+        return NetUtils.getToken(params[0], params[1]);
+    }
+}
